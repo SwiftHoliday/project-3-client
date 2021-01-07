ba5c71a191686bdf8f7bd718dc91eec199e7ff60
Jonathyn Carter <jonathyncarter@gmail.com>
2021-01-06 20:21:25 -0500

    adds merge for master

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import './App.css';
 import Header from'./components/Header'
 import Footer from './components/Footer'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import DashboardPage from './pages/DashboardPage'
+import SignupPage from './pages/SignupPage'
+
+import { Switch, Route } from 'react-router-dom';
+
+import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <p>Project 3</p>
+      <main>
+        <Switch>
+          <Route exact path="/" render={props =>
+            <HomePage />
+          } />
+          <Route exact path="/dashboard" render={props =>
+            <DashboardPage />
+          } />
+          <Route exact path="/signup" render={props =>
+            <SignupPage />
+          } />
+          <Route exact path="/Login" render={props =>
+            <LoginPage />
+          } />
+          </Switch>
+      </main>
       <Footer />
     </div>
   );
