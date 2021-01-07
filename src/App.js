88b024707ee253938a4bbb32375df7de9e25f5be
Jonathyn Carter <jonathyncarter@gmail.com>
2021-01-06 19:25:46 -0500

    imports header and footerr

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
 import './App.css';
+import Header from'./components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <p>Project 3</p>
+      <Footer />
     </div>
   );
 }
