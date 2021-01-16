cb4b961a192c3411ade3d893e4909396318a8118
Jonathyn Carter <jonathyncarter@gmail.com>
2021-01-16 17:46:54 -0500

    adds env folder for api key APP WORKING

--- src/services/news-api.js
+++ src/services/news-api.js
@@ -1,13 +1,7 @@
-const BASE_URL = 'http://localhost:3001/api/newsAPI';
-    
+const API_TOKEN = process.env.REACT_APP_NEWS_API_KEY;
+const BASE_URL = 'http://newsapi.org/v2/'
 export function getNews() {
-    return fetch(BASE_URL + '/get-news', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'Application/json'
-        }, body: {}
-    }
-    )
+    return fetch(BASE_URL + 'top-headlines?country=us&apiKey=' + API_TOKEN)
         .then((res) => res.json());
     }
     
