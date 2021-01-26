6f8eed4ecf022b5c2c772adf658c5e697d3783e8
Jonathyn Carter <jonathyncarter@gmail.com>
2021-01-26 11:46:07 -0500

    prepares for deployment

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,6 @@
 import { setToken, getUserFromToken, removeToken } from './tokenService';
-const BASE_URL = 'http://localhost:3001/api/users';
+// const BASE_URL = 'http://localhost:3001/api/users'; development url
+const BASE_URL = 'https://what-in-the-world-app.herokuapp.com/api/users';
 
 
 
